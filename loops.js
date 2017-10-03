f562d3df1f168fd5ee62e821a855c58b3be23cf8
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-03 01:38:33 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,25 @@
+function forLoop(array) {
+  var finalArray = [];
+  for (let i = 0; i < 25; i++) {
+    if (i === 0 || i === 1) {
+      finalArray.push("I am 1 strange loop.")
+    } else {
+      finalArray.push("I am " + i + " strange loops.")
+    }
+  }
+  return finalArray;
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n--);
+  }
+  return 'done';
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+ 
+}
