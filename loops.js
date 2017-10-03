a12b44fba815b3686414c44fde9c736ce2ec4096
mattfaircloth <mattcfaircloth@gmail.com>
2017-10-03 01:49:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -25,7 +25,7 @@ function doWhileLoop(array) {
   return Math.random() >= 0.5
   }
   do {
-    return array.pop();
+    array.pop();
   } while (array.length > 0 && maybeTrue())
-
+ return array;
 }
